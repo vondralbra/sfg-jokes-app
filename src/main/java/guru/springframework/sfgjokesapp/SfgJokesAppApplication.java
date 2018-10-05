8d4bc19d766619ccdeb893dc125277e028149973
Philip Alb <philip.alb@gmx.de>
2018-10-05 09:51:54 +0200

    Create initial project setup for #1

---
+++ src/main/java/guru/springframework/sfgjokesapp/SfgJokesAppApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.sfgjokesapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgJokesAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgJokesAppApplication.class, args);
+	}
+}
