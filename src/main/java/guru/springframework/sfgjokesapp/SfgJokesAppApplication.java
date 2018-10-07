5768ef2c31c0f6d6fe0ebc5046b15e770758cfd2
Philip Alb <philip.alb@gmx.de>
2018-10-07 08:29:06 +0200

    Change Bean detection from Java-based to XML based configuration.

--- src/main/java/guru/springframework/sfgjokesapp/SfgJokesAppApplication.java
+++ src/main/java/guru/springframework/sfgjokesapp/SfgJokesAppApplication.java
@@ -2,10 +2,11 @@ package guru.springframework.sfgjokesapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
-@ComponentScan(basePackages = { "guru.springframework" })
+//@ComponentScan(basePackages = { "guru.springframework" })
+@ImportResource("classpath:chuck-config.xml")
 public class SfgJokesAppApplication {
 
 	public static void main(String[] args) {
--- src/main/java/guru/springframework/sfgjokesapp/config/ChuckConfiguration.java
+++ src/main/java/guru/springframework/sfgjokesapp/config/ChuckConfiguration.java
@@ -1,14 +1,14 @@
 package guru.springframework.sfgjokesapp.config;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
-@Configuration
+//@Configuration
 public class ChuckConfiguration {
 
-	@Bean
+	// @Bean
 	public ChuckNorrisQuotes chuckNorrisQuotes() {
 		return new ChuckNorrisQuotes();
 	}
